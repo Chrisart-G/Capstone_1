9b5323fb7cdf3bf7bf059b0fa106e092fbb50729
Chris-G <09xghagox09@gmail.com>
2025-04-08 00:03:32 +0800

    revert

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,50 +1,31 @@
+
 import './App.css';
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, BrowserRouter } from 'react-router-dom';
 import Logins from './Component/Logins/Login';
 import Singnup from './Component/Signup/Signup';
 import MunicipalLandingPage from './Component/Home/Home';
 import MayorsPermitForm from './Component/Form/businesspermit/businesspermit';
-import PermitsHomepage from './Component/Home/Requesthome';
-import AdminDashboard from './Component/Adminhomepage/Adminhome';
-import ProtectedRoute from './Component/Auth/ProtectedRoute'; 
+import PermitsHomepage from'./Component/Home/Requesthome';
+import AdminDashboard from'./Component/Adminhomepage/Adminhome';
+
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          {/* Public routes */}
-          <Route path="/" element={<Navigate to="/Login" />} />
-          <Route path="/Login" element={<Logins />} />
-          <Route path="/Sign-up" element={<Singnup />} />
-          
-          {/* Protected routes */}
-          <Route path="/Chome" element={
-            <ProtectedRoute>
-              <MunicipalLandingPage />
-            </ProtectedRoute>
-          } />
-          <Route path="/Uform" element={
-            <ProtectedRoute>
-              <MayorsPermitForm />
-            </ProtectedRoute>
-          } />
-          <Route path="/Permits" element={
-            <ProtectedRoute>
-              <PermitsHomepage />
-            </ProtectedRoute>
-          } />
-          <Route path="/AdminDash" element={
-            <ProtectedRoute>
-              <AdminDashboard />
-            </ProtectedRoute>
-          } />
-          
-          
-          <Route path="*" element={<Navigate to="/Login" />} />
-        </Routes>
-      </Router>
+   <Router>
+          <Routes>
+            <Route path="/Login" element={<Logins />} />
+            <Route path="/Sign-up"y element={<Singnup />} />
+            <Route path="/Chome" element={<MunicipalLandingPage />} />
+            <Route path="/Uform" element={<MayorsPermitForm/>} />
+            <Route path="/Permits" element={<PermitsHomepage/>} /> 
+            <Route path="/AdminDash" element={<AdminDashboard/>} />
+          {/* amo si chongo */}
+          </Routes>
+    </Router>
+  
+    
     </div>
   );
 }
